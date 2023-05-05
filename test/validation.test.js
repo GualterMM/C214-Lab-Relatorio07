b7b3bce62d70003d368b251d84b586aa57144119
Gualter Mesquita <gualter.mesquita@leucotron.com.br>
2023-05-05 17:17:34 -0300

    Criação de testes unitários e teste mock.

--- test/validation.test.js
+++ test/validation.test.js
@@ -3,17 +3,29 @@ const Validation = require('../src/utils/validation');
 
 it('Caso válido', () => {
     const result = Validation.create({
-        nome: "Iago",
-        email: "iago.luiz@ges.inatel.br",
-        senha: "123456789"
+        marca: "Volks",
+        cor: "Azul",
+        ano: "2020",
+        tipo: "Casual"
     });
     expect(result).toEqual(undefined);
 });
 
-it('Caso inválido - sem o parâmetro nome', () => {
+it('Caso inválido - tipo vazio', () => {
     const result = Validation.create({
-        email: "iago.luiz@ges.inatel.br",
-        senha: "123456789"
+        marca: "Volks",
+        cor: "Azul",
+        ano: "2020",
+        tipo: ""
+    });
+    expect(result.name).toEqual(Constants.ErrorValidation.name);
+});
+
+it('Caso inválido - sem o parâmetro cor', () => {
+    const result = Validation.create({
+        marca: "Volks",
+        ano: "2020",
+        tipo: "Esporte"
     });
     expect(result.name).toEqual(Constants.ErrorValidation.name);
 });
